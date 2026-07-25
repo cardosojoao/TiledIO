770532e76376bb8caf1f3c8e40e5b5557a939811
retnz <retnz80@gmail.com>
2026-07-25 23:15:00 +0100

    add project additional properties

--- Models/Project.cs
+++ Models/Project.cs
@@ -6,9 +6,49 @@ namespace TiledIO.Models
 
     public class Project
     {
+        [JsonPropertyName("automappingRulesFile")]
+        public string AutomappingRulesFile { get; set; } = string.Empty;
+
+        [JsonPropertyName("commands")]
+        public List<Command> Commands { get; set; } = new();
+
+        [JsonPropertyName("extensionsPath")]
+        public string ExtensionsPath { get; set; } = string.Empty;
+
+        [JsonPropertyName("folders")]
+        public List<string> Folders { get; set; } = new();
+
         [JsonPropertyName("properties")]
         public List<Property> Properties { get; set; }
         [JsonPropertyName("propertyTypes")]
         public List<PropertyType> PropertyTypes { get; set; }
+    }
+
+
+    public class Command
+    {
+        [JsonPropertyName("arguments")]
+        public string Arguments { get; set; } = string.Empty;
+
+        [JsonPropertyName("command")]
+        public string Executable { get; set; } = string.Empty;
+
+        [JsonPropertyName("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("saveBeforeExecute")]
+        public bool SaveBeforeExecute { get; set; }
+
+        [JsonPropertyName("shortcut")]
+        public string? Shortcut { get; set; }
+
+        [JsonPropertyName("showOutput")]
+        public bool ShowOutput { get; set; }
+
+        [JsonPropertyName("workingDirectory")]
+        public string WorkingDirectory { get; set; } = string.Empty;
     }
 }
